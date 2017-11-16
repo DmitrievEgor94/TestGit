f42f03334ed0bac21883131988ac2b0db7d45ea6
Dmitriev Egor <DmitrievEgor94@yandex.ru>
2017-11-16 20:52:32 +0400

    Added MessageWriter#write

--- src/main/java/com/mycompany/MessageWriter.java
+++ src/main/java/com/mycompany/MessageWriter.java
@@ -1,4 +1,19 @@
 package com.mycompany;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class MessageWriter {
+
+    void get(String file, String message) throws FileNotFoundException {
+
+        try (PrintWriter printWriter = new PrintWriter(new File(file))) {
+
+            printWriter.print(message);
+            printWriter.flush();
+
+        }
+
+    }
 }
