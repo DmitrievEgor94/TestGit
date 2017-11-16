1112cc97a099dcee5d74040b8c938201270dc35e
Dmitriev Egor <DmitrievEgor94@yandex.ru>
2017-11-16 20:59:08 +0400

    Deleted Main#print and Main#main

--- src/main/java/com/mycompany/Main.java
+++ src/main/java/com/mycompany/Main.java
@@ -3,12 +3,5 @@ package com.mycompany;
 public class Main {
     
     private static String NAME_OF_FILE = "test.txt";
-
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-
-    private static void print(){
-        System.out.println("Hello world");
-    }
+    
 }
